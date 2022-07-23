87fe2e6327a8f959d26eff4f47a137c212903621
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-07-23 12:33:01 +0900

    Create binary_gqp.js

---
+++ binary_gqp.js
@@ -0,0 +1,5 @@
+function solution(N) {
+  const binary = N.toString(2);
+	const trimmed = binary.substr(0, binary.lastIndexOf('1') + 1);
+	return Math.max(...(trimmed.split('1').map(item => item.length)));
+}
