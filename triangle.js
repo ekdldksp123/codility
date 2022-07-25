63f1160576124ac3fa22471424de71cd583c76fe
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-07-26 01:47:11 +0900

    Create triangle.js

---
+++ triangle.js
@@ -0,0 +1,12 @@
+function solution(A) {
+    if(A.length < 3) return 0
+
+    A = A.sort((a,b) => a-b)
+    for (let i = 0; i < A.length-2; i++){
+        if (A[i]+A[i+1] > A[i+2] && A[i+1]+A[i+2] > A[i] && A[i]+A[i+2] > A[i+1]) {
+            return 1
+        }
+    }
+
+    return 0
+}
